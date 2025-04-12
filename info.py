2012b9dc56b0b7132b45b0d6d5d43e565272c8ab
TR Crazy <131470429+DreamerNo1@users.noreply.github.com>
2025-04-12 13:53:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://TRCrazy:multi@2010@cluster0.zsnpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb://rctestuser:mymongopassword@mongo:27017/rocketchat?authSource=admin")   # IF Multiple Database Is False Then Fill Only This Database Url.
 DATABASE_NAME = environ.get('DATABASE_NAME', "tjfilterbot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'TJcollection')
 
