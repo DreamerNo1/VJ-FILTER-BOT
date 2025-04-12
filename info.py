6542fc9bf212f65f585e3a4f58acdca0e766bec1
TR Crazy <131470429+DreamerNo1@users.noreply.github.com>
2025-04-12 17:12:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "7976040870:AAHHyybKIc-0Udq8YfW5NiRz-07iV72
 
 
 # This Pictures Is For Start Message Picture, You Can Add Multiple By Giving One Space Between Each.
-PICS = (environ.get('PICS', 'https://wallpapers.com/images/hd/strongest-soldier-levi-ackerman-8ci33tu3v6gjmge8.jpg')).split()
+PICS = (environ.get('PICS', 'https://i.ibb.co/tpwvfPss/x.jpg')).split()
 
 
 # Admins & Users
